d6f371947feab7b8bc2b1ee23b437b95cce00608
fzbo <elizabeth.bazan@gmail.com>
2018-04-01 11:49:35 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//DEPENDENCIES
+const express = require('express');
+const mongoos = require('mongoose');
+
+//EXPRESS SERVER INSTANCE
+const app = express();
+
+app.get(' / ', (req, res) => {
+    res.send('It Works!');
+});
+
+const port = process.env.PORT || 5000;
+
+//THIS WILL CONFIRM SERVER IS LIVE BY GOING LOCAL HOST: 5000. 
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
