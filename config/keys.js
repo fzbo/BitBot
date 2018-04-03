01831c3746cbf5f5b304405645ec2f23522cc10c
fzbo <elizabeth.bazan@gmail.com>
2018-04-02 22:25:57 -0400

    Update to include keys and passport files

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://canelita:mLabs$18@ds121588.mlab.com:21588/bitbot-dev ',
+    mongoURI: 'mongodb://canelita:mLab$18@ds121588.mlab.com:21588/bitbot-dev ',
     googleClientID: '258981796855-uftgtajtgbi58qhgti9vovq7kcua7fil.apps.googleusercontent.com ',
     googleClientSecret: 'KT0UiDhvjhYHqZBtWqvJ7lZ5 ',
 } 
