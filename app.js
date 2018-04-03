528cb5b390f06e02c74ac0101d174474bfe91ef9
fzbo <elizabeth.bazan@gmail.com>
2018-04-02 22:28:18 -0400

    Update to include keys and passport file

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-
-
 //DEPENDENCIES
 const express = require('express');
 const mongoos = require('mongoose');
+const passport = require('passport');
+
+//PASSPORT CONFIG
+require('./config/passport')(passport);
+
+
+//LINK /LOAD OAUTH ROUTES
+const auth = require('./routes/auth');
+
 
 //EXPRESS SERVER INSTANCE
 const app = express();
@@ -11,6 +18,10 @@ app.get(' / ', (req, res) => {
     res.send('It Works!');
 });
 
+//USE ROUTES
+app.use('/auth', auth);
+
+//ALLOWS APP TO RUN IN EITHER SPECIFIED ENVIORNMENT OR ON DEDICATED PORT #
 const port = process.env.PORT || 5000;
 
 //THIS WILL CONFIRM SERVER IS LIVE BY GOING LOCAL HOST: 5000. 
