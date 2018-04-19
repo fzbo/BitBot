36fba2f71fbe99d4139a9ed038926cd4cc919ffd
fzbo <elizabeth.bazan@gmail.com>
2018-04-18 20:05:12 -0400

    Feature: App added

---
+++ config/passport.js
@@ -0,0 +1,52 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+// LOAD USER MODEL
+const User = mongoose.model('users');
+
+module.exports = function(passport){
+  passport.use(
+    new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    }, (accessToken, refreshToken, profile, done) => {
+      // console.log(accessTosken);
+      // console.log(profile);
+
+      const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+      
+      const newUser = {
+        googleID: profile.id,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        email: profile.emails[0].value,
+        image: image
+      }
+
+      // CHECK FOR EXISTING USER
+      User.findOne({
+        googleID: profile.id
+      }).then(user => {
+        if(user){
+          // RETURN USER
+          done(null, user);
+        } else {
+          // CREATE USER
+          new User(newUser)
+            .save()
+            .then(user => done(null, user));
+        }
+      })
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
+}
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//CREATE SCHEMA
+const UserSchema = new Schema ({
+    googleID: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    image: {
+        type: String
+    }
+
+});
+
+//CREATE COLLECTION, AND ADD SCHEMA
+mongoose.model('users', UserSchema);
